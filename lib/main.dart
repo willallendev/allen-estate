2bc4ca35667f3a0912413727e9e4aa0d4ae049e8
Will <allenwilliamdev@gmail.com>
2020-07-02 11:08:49 -0400

    [NOTICKET] - Remove comment at main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/foundation.dart' as Foundation;
 import 'package:flutter/material.dart';
 
 void main() {
-//  runApp(AllenEstateApp());
   runApp(
     DevicePreview(
       usePreferences: true,
