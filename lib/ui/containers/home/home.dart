127c81111e2c2eae0cc46a5d63715ef3086e7879
Will <allenwilliamdev@gmail.com>
2020-07-09 12:26:00 -0400

    [feature/WA/ARDV-6] - Move auto generated app to its own container

---
+++ lib/ui/containers/home/home.dart
@@ -0,0 +1,9 @@
+import 'package:allenrealestateflutter/ui/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class HomeContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return HomePage(title: 'Flutter Demo Home Page');
+  }
+}
