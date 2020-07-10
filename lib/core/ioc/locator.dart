9720492527d54bf1ba16abde54737aef0cbb9f5b
Will <allenwilliamdev@gmail.com>
2020-07-10 12:04:08 -0400

    [feature/WA/ARDV-6] - Setup service locator

---
+++ lib/core/ioc/locator.dart
@@ -0,0 +1,8 @@
+import 'package:allenrealestateflutter/core/ioc/locator.iconfig.dart';
+import 'package:get_it/get_it.dart';
+import 'package:injectable/injectable.dart';
+
+final locator = GetIt.instance;
+
+@injectableInit
+void setupLocator() => $initGetIt(locator);
