d3b239f23eb77bcaee7fde74a85d8824cbd14e7b
Will <allenwilliamdev@gmail.com>
2020-07-24 13:09:12 -0400

    [feature/WA/ARDV-8] - Refactor info icons widgets

--- lib/ui/widgets/info_icon/info_icon.dart
+++ lib/ui/widgets/info_icon/info_icon.dart
@@ -1,32 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class InfoIcon extends StatelessWidget {
   final String info;
-  final Widget icon;
+  final double fontSize;
+  final double textIconSpace;
+  final FaIcon icon;
 
-  InfoIcon({@required this.info, @required this.icon})
-      : assert(info != null, '"info" param cannot be null'),
-        assert(icon != null, '"icon" param cannot be null');
+  InfoIcon({
+    @required this.info,
+    this.fontSize = 10,
+    this.textIconSpace = 2,
+    @required this.icon,
+  })  : assert(info != null, '"info" param cannot be null'),
+        assert(icon != null, '"icon" param cannot be null'),
+        assert(fontSize != null, '"fontSize" param cannot be null'),
+        assert(textIconSpace != null, '"textIconSpace" param cannot be null');
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return Container(
-      constraints: BoxConstraints(
-        maxWidth: 64,
-      ),
       child: Column(
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           icon,
-          Container(height: 4),
+          Container(height: textIconSpace),
           Text(
             info,
             maxLines: 1,
             overflow: TextOverflow.fade,
             style: TextStyle(
               color: theme.textTheme.headline1.color,
+              fontWeight: FontWeight.w500,
+              fontSize: fontSize,
             ),
           ),
         ],
