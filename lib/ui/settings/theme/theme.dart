46d8cd2851cc0020236401589d68cf719ea64620
Will <allenwilliamdev@gmail.com>
2020-07-25 18:00:23 -0400

    [feature/WA/ARDV-10] - Add generator for idle search app bar

--- lib/ui/settings/theme/theme.dart
+++ lib/ui/settings/theme/theme.dart
@@ -6,7 +6,8 @@ final _primaryColorVariant = Colors.teal.shade700;
 final _secondaryColor = Color.fromRGBO(252, 130, 55, 0.87);
 final _backgroundColor = Colors.white;
 final _foregroundColor = Colors.white;
-const _darkFont = Color.fromRGBO(0, 0, 0, 0.87);
+final _darkFont = Colors.black.withOpacity(.87);
+final _darkFontDimmed = Colors.black.withOpacity(.6);
 final _baseTextTheme = GoogleFonts.robotoTextTheme();
 final _textTheme = _baseTextTheme.copyWith(
   headline1: _baseTextTheme.headline1.copyWith(color: _darkFont),
@@ -15,17 +16,30 @@ final _textTheme = _baseTextTheme.copyWith(
   headline4: _baseTextTheme.headline4.copyWith(color: _darkFont),
   headline5: _baseTextTheme.headline5.copyWith(color: _darkFont),
   headline6: _baseTextTheme.headline6.copyWith(color: _darkFont),
-  subtitle1: _baseTextTheme.subtitle1.copyWith(color: _darkFont),
-  subtitle2: _baseTextTheme.subtitle2.copyWith(color: _darkFont),
-  bodyText1: _baseTextTheme.bodyText1.copyWith(color: _darkFont),
-  bodyText2: _baseTextTheme.bodyText2.copyWith(color: _darkFont),
-  caption: _baseTextTheme.caption.copyWith(color: _darkFont),
-  overline: _baseTextTheme.overline.copyWith(color: _darkFont),
+  subtitle1: _baseTextTheme.subtitle1.copyWith(color: _darkFontDimmed),
+  subtitle2: _baseTextTheme.subtitle2.copyWith(color: _darkFontDimmed),
+  bodyText1: _baseTextTheme.bodyText1.copyWith(color: _darkFontDimmed),
+  bodyText2: _baseTextTheme.bodyText2.copyWith(color: _darkFontDimmed),
+  caption: _baseTextTheme.caption.copyWith(color: _darkFontDimmed),
+  overline: _baseTextTheme.overline.copyWith(color: _darkFontDimmed),
   button: _baseTextTheme.button.copyWith(color: _darkFont),
+);
+final _appBarTheme = AppBarTheme(
+  elevation: 0,
+  color: Colors.white,
+  brightness: Brightness.light,
+  textTheme: _textTheme,
+);
+final _iconTheme = IconThemeData(
+  color: _darkFontDimmed,
+  size: 17,
 );
 
 ThemeData buildAppTheme() {
   return ThemeData(
+    appBarTheme: _appBarTheme,
+    textTheme: _textTheme,
+    iconTheme: _iconTheme,
     primarySwatch: _primaryColor,
     primaryColor: _primaryColor,
     accentColor: _secondaryColor,
@@ -33,7 +47,6 @@ ThemeData buildAppTheme() {
     backgroundColor: _backgroundColor,
     cardColor: _foregroundColor,
     visualDensity: VisualDensity.adaptivePlatformDensity,
-    textTheme: _textTheme,
     buttonColor: _primaryColor,
   );
 }
