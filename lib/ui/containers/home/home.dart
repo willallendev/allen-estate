76400d6fcb3913994a9da4ff6c259741fef9d824
Will <allenwilliamdev@gmail.com>
2020-08-03 16:06:11 -0400

    [feature/WA/ARDV-10] - Add navigate on query submit

--- lib/ui/containers/home/home.dart
+++ lib/ui/containers/home/home.dart
@@ -1,9 +1,9 @@
-import 'dart:developer';
-
 import 'package:allenrealestateflutter/core/view_models/home_view_model/home_view_model.dart';
 import 'package:allenrealestateflutter/ui/containers/base_container/base_container.dart';
 import 'package:allenrealestateflutter/ui/screens/home/home.dart';
+import 'package:allenrealestateflutter/ui/settings/router/router.gr.dart';
 import 'package:allenrealestateflutter/ui/utils/re_search_delegate.dart';
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
 class HomeContainer extends StatelessWidget {
@@ -11,7 +11,12 @@ class HomeContainer extends StatelessWidget {
 
   void _onSearch(BuildContext context) async {
     String query = await showSearch<String>(context: context, delegate: ReSearchDelegate());
-    log(query ?? 'empty', name: tag);
+    if (query != null) {
+      ExtendedNavigator.of(context).push(
+        Routes.reSearchResults,
+        queryParams: {'query': query},
+      );
+    }
   }
 
   @override
