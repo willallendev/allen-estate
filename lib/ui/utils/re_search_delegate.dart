cf1fc627b826f7f28bdd34a93422514f521174ff
Will <allenwilliamdev@gmail.com>
2020-08-03 14:40:08 -0400

    [feature/WA/ARDV-10] - Update RE search delegate

---
+++ lib/ui/utils/re_search_delegate.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class ReSearchDelegate extends SearchDelegate<String> {
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return <Widget>[
+      CloseButton(onPressed: () => query = ''),
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return BackButton(onPressed: () => close(context, null));
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    final trimQuery = query.trim();
+    if (trimQuery != '') {
+      close(context, query);
+    }
+    return Container();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return Container();
+  }
+}
