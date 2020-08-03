f53de9e2796cd8ef23e178600954498ac61904ba
Will <allenwilliamdev@gmail.com>
2020-08-03 00:34:38 -0400

    [feature/WA/ARDV-11] - Add container

---
+++ lib/ui/containers/re_search_results/re_search_results.dart
@@ -0,0 +1,9 @@
+import 'package:allenrealestateflutter/ui/screens/under_development/under_development.dart';
+import 'package:flutter/material.dart';
+
+class ReSearchResultsContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return UnderDevelopmentScreen(title: 'ReSearchResultsContainer');
+  }
+}
--- lib/ui/settings/router/router.dart
+++ lib/ui/settings/router/router.dart
@@ -1,10 +1,12 @@
 import 'package:allenrealestateflutter/ui/containers/home/home.dart';
+import 'package:allenrealestateflutter/ui/containers/re_search_results/re_search_results.dart';
 import 'package:auto_route/auto_route_annotations.dart';
 
 @AdaptiveAutoRouter(
   routes: <AutoRoute>[
     // initial route is named "/"
     AdaptiveRoute(page: HomeContainer, initial: true, name: 'home'),
+    AdaptiveRoute(page: ReSearchResultsContainer, name: 'reSearchResults', path: '/real-estate-results'),
   ],
 )
 class $Router {}
