359da56bf64df72c19cde70aca2010814e7f34c2
Will <allenwilliamdev@gmail.com>
2020-08-17 23:24:10 -0400

    [feature/WA/ARDV-12] - Connect to view model

--- lib/ui/containers/re_single/re_single.dart
+++ lib/ui/containers/re_single/re_single.dart
@@ -1,4 +1,6 @@
-import 'package:allenrealestateflutter/ui/screens/under_development/under_development.dart';
+import 'package:allenrealestateflutter/core/view_models/re_single/re_single.dart';
+import 'package:allenrealestateflutter/ui/containers/base_container/base_container.dart';
+import 'package:allenrealestateflutter/ui/screens/re_single/re_single.dart';
 import 'package:auto_route/auto_route_annotations.dart';
 import 'package:flutter/material.dart';
 
@@ -12,8 +14,16 @@ class ReSingleContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return UnderDevelopmentScreen(
-      title: '$tag: \nid: $id',
+    return BaseContainer(
+      onModelReady: (ReSingleViewModel model) {
+        model.getSingleData(id);
+      },
+      builder: (BuildContext context, ReSingleViewModel model, Widget child) {
+        return ReEstateSingleScreen(
+          realEstate: model.mainState.data.realEstate,
+          carouselRealEstateList: model.mainState.data.carouselData,
+        );
+      },
     );
   }
 }
