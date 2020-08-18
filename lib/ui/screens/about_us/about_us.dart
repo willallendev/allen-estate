afc2bba8f4040c97aff43ffb8ad4eff6d373a13a
Will <allenwilliamdev@gmail.com>
2020-08-18 16:23:06 -0400

    [feature/WA/ARDV-15] - Add screen

--- lib/ui/screens/about_us/about_us.dart
+++ lib/ui/screens/about_us/about_us.dart
@@ -1,15 +1,47 @@
+import 'package:allenrealestateflutter/core/data_models/async_state.dart';
 import 'package:allenrealestateflutter/ui/utils/app_bar_generators.dart';
+import 'package:allenrealestateflutter/ui/widgets/async_state_manager/async_state_manager.dart';
+import 'package:allenrealestateflutter/ui/widgets/custom_bottom_navigation_bar/custom_bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-class AboutUsScreen extends StatelessWidget {
+class AboutUsScreen extends StatefulWidget {
+  @override
+  _AboutUsScreenState createState() => _AboutUsScreenState();
+}
+
+class _AboutUsScreenState extends State<AboutUsScreen> {
+  AsyncState _state = AsyncState.loading;
+  WebViewController _controller;
+
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    final textTheme = theme.textTheme;
+    void _setLoading(bool isLoading) {
+      setState(() {
+        _state = isLoading ? AsyncState.loading : AsyncState.done;
+      });
+    }
+
     return Scaffold(
       backgroundColor: theme.backgroundColor,
       appBar: generateSimpleAppBar(context: context, title: 'About us', elevation: 0, iconTheme: theme.iconTheme),
-	    body: ,
+      bottomNavigationBar: CustomBottomNavigationBar(currentIndex: 1),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          WebView(
+            initialUrl: 'https://github.com/willallendev',
+            javascriptMode: JavascriptMode.unrestricted,
+            onPageStarted: (url) => _setLoading(true),
+            onPageFinished: (url) => _setLoading(false),
+          ),
+          AsyncStateManager(
+            state: _state,
+            child: Container(),
+          )
+        ],
+      ),
     );
   }
 }
