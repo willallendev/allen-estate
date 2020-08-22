b17a3f8fdcaa00782652379e726e956050f40ff1
Will <allenwilliamdev@gmail.com>
2020-08-21 23:24:56 -0400

    [NOTICKET] - Add locator env variables

--- lib/core/ioc/locator.dart
+++ lib/core/ioc/locator.dart
@@ -2,7 +2,10 @@ import 'package:allenrealestateflutter/core/ioc/locator.config.dart';
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 
+const dev = Environment('dev');
+const test = Environment('test');
+const prod = Environment('prod');
 final locator = GetIt.instance;
 
 @injectableInit
-void setupLocator() => $initGetIt(locator);
+void setupLocator() => $initGetIt(locator, environment: 'prod');
